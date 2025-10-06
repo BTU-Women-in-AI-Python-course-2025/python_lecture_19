4257a8bc78d5d4ffa91af04d751658382fa76f63
MariamKipshidze <mari.kifshidze@gmail.com>
2025-10-06 20:55:35 +0400

    Django admin - list_editable

--- Lecture code 1/blog_post/blog/admin.py
+++ Lecture code 1/blog_post/blog/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from blog.models import BlogPost, BlogPostImage, Author, BannerImage
 
-admin.site.register(BlogPost)
+
 admin.site.register(BlogPostImage)
 admin.site.register(Author)
 admin.site.register(BannerImage)
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'create_date', 'update_date', 'active', 'deleted')
+    list_editable = ('deleted',)
+    list_filter = ('active', 'deleted', 'create_date', 'update_date', 'category')
+    search_fields = ('title', 'text')
+    filter_horizontal = ('authors',)
+    readonly_fields = ('create_date', 'update_date')
+    ordering = ('title',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'text', 'authors', 'category', 'website', 'document')
+        }),
+        ('Status', {
+            'fields': ('active', 'deleted')
+        }),
+        ('Timestamps', {
+            'fields': ('create_date', 'update_date'),
+        }),
+    )
